159407c00b3cfc1d9f44c8e61aaf16fd071ab08c
ckovach <ckovach@4e33aaf1-3bdf-43e8-baf7-e7e736085cc8>
2018-02-07 22:52:12 +0000

    Reconstruct an input signal based on the output struct returned by bsident.m (possibly from another signal)
    git-svn-id: https://saccade.neurosurgery.uiowa.edu/svn/KovachToolbox/trunk/BSTD@977 4e33aaf1-3bdf-43e8-baf7-e7e736085cc8

---
+++ filtrecon.m
@@ -0,0 +1,76 @@
+
+
+function out=filtrecon(x,bsid)
+
+% out = bsident(x,segment,lpfilt,ncomp,varargin)
+%
+% Reconstruct an input based on the output of bsident. 
+%
+% See also BSIDENT
+%
+% C. Kovach 2018
+
+%%% Method to identify impulse
+%impulse_method = 'zthreshold';
+impulse_method = 'iterz';
+thresh = 3;
+%impulse_method = 'kmeans';
+
+
+apply_fun = @(x)x.^3;
+
+xresid = x;
+
+Xrec = zeros(size(x) );
+Xfilt = zeros(size(x) );
+Ximp = zeros(size(x) );
+
+for k = 1:length(bsid)
+%%
+      n = size(xresid,1);
+     for kk = 1:size(x,2)
+        xx = xresid(:,kk);
+       
+
+         f = bsid(k).f;
+         nX = length(f);
+         f(n)=0;
+         f = circshift(f,-floor(nX/2));
+
+         h = bsid(k).BFILT;
+         h(n) = 0;
+         h = circshift(h,-floor(nX/2));
+
+         xfilt = ifft(fft(xx).*fft(h));
+
+
+        switch impulse_method
+            case 'zthreshold'
+                ximp = zscore(apply_fun(xfilt))>thresh;
+            case 'iterz'
+                ximp = isnan(iterz(apply_fun(xfilt),thresh,1));
+                
+            case 'kmeans'
+                kmx = apply_fun(xfilt);
+                [km,kmc]  = kmeans(kmx + 0./(zscore(kmx)>0),2);
+    %            [km,kmc]  = kmeans(xfilt + 0./(zscore(xfilt)>1),2);
+                [~,mxi] = max(kmc);
+                ximp = km==mxi;
+        end
+        xrec = ifft(fft(xfilt.*ximp).*fft(f));%*nX/n;
+        a = xrec'*xresid./sum(xrec.^2);
+        xrec = a*xrec;
+        Xrec(:,kk) = xrec;
+        Xfilt(:,kk) = xfilt;
+        Ximp(:,kk) = ximp;
+    end
+     out(k).xrec = Xrec; %#ok<*AGROW>
+     out(k).xfilt = Xfilt;
+     out(k).ximp = sparse(Ximp);
+    out(k).a = a;
+    out(k).exvar = 1-sum(abs(xresid-xrec).^2)./sum(abs(xresid).^2);
+      xresid =xresid-Xrec;
+
+end
+    
+    
