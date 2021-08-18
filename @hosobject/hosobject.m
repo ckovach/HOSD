afaefb0c43320eeefa5e46614792d2cc37ee6a39
ckovach <ckovach@4e33aaf1-3bdf-43e8-baf7-e7e736085cc8>
2021-08-18 23:42:01 +0000

    Minor change
    git-svn-id: https://saccade.neurosurgery.uiowa.edu/svn/KovachToolbox/trunk/BSTD@1748 4e33aaf1-3bdf-43e8-baf7-e7e736085cc8

--- @hosobject/hosobject.m
+++ @hosobject/hosobject.m
@@ -525,7 +525,7 @@ classdef hosobject < handle
            me.win = window(me.wintype,N); 
            me.radw = ifftshift((0:me.fftN - 1 )' - floor((me.fftN)/2))/(me.fftN)*2*pi;
            me.sampt = ifftshift((0:me.fftN - 1 ) - floor((me.fftN)/2)'); 
-           if (N ~= Norig || length(me.freqs{1})~=me.fftN) && me.do_indexing_update
+           if me.do_indexing_update && (N ~= Norig || length(me.freqs{1})~=me.fftN) 
                 freqs = {me.fftfreq(me.fftN)*me.sampling_rate};
                 freqs(1:me.order) = freqs;
                 me.freqs = freqs;
