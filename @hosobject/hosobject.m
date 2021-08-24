984fe2f521aa897dd5f9c50a6925c90156672515
ckovach <ckovach@4e33aaf1-3bdf-43e8-baf7-e7e736085cc8>
2021-08-24 04:14:02 +0000

    Remove debugging line
    git-svn-id: https://saccade.neurosurgery.uiowa.edu/svn/KovachToolbox/trunk/BSTD@1764 4e33aaf1-3bdf-43e8-baf7-e7e736085cc8

--- @hosobject/hosobject.m
+++ @hosobject/hosobject.m
@@ -317,9 +317,9 @@ classdef hosobject < handle
                    if isprop(me(1),fns{k})
                         try
                        me(1).(fns{k}) = obj(1).(fns{k});
-                       if me(1).lag~=obj(1).lag
-                           keyboard
-                       end
+%                        if me(1).lag~=obj(1).lag
+%                            keyboard
+%                        end
                         catch
                         end
                    end
