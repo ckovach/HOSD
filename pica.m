1707b133c5056665a0a29cbd0874d6b2338a709e
ckovach <ckovach@4e33aaf1-3bdf-43e8-baf7-e7e736085cc8>
2022-03-02 23:30:13 +0000

    Some updates
    git-svn-id: https://saccade.neurosurgery.uiowa.edu/svn/KovachToolbox/trunk/BSTD@1826 4e33aaf1-3bdf-43e8-baf7-e7e736085cc8

--- pica.m
+++ pica.m
@@ -1,13 +1,17 @@
 
-function A = pica(X0,ord,a0)
+function A = pica(X0,ncomp,ord,a0,dnl)
 
 %ICA through power iteration
 
-if nargin < 2 || isempty(ord)
+if nargin < 2 || isempty(ncomp)
+    ncomp = size(X0,2);
+end
+
+if nargin < 3 || isempty(ord)
     ord = 4;
 end
 
-if nargin < 3 || isempty(A0)
+if nargin < 4 || isempty(a0)
     a0 = randn(size(X0,2),1);
 end
 
@@ -21,9 +25,18 @@ Z0 = (X0-m0)*pinv(WM);
 
 X = Z0;
 
+if isnumeric(ord)
+    nl = @(x)x.^ord;
+    dnl = @(x)ord*x.^(ord-1);
+elseif ischar(ord)
+    switch ord
+        case 'logcosh'
+            nl = @(x)log(cosh(x));
+            dnl = @(x)tanh(x);
+    end
+end
 
-
-for dim = 1:size(X,2)
+for dim = 1:ncomp
 
 R = X'*X;
 Rinv = pinv(R);
@@ -33,9 +46,9 @@ Rinv = pinv(R);
 tol = 1e-6;
 d = Inf;
 
-a = a0;
+a = WM*a0;
 
-cm = cumulant(X*a,ord);
+% cm = cumulant(X*a,ord);
 
 iter = 1;
 clear ds kt sk
@@ -43,18 +56,20 @@ while d> tol && iter < maxiter
     
     r = X*a;
 
-%     rX = r.^(ord-2).*X; 
-%     G = (rX'*X)*Rinv;
-     rX = r.^((ord-2)/2).*X; 
-     G = (rX'*rX)*Rinv;
-    
-%       [anew,ev(iter)] = eigs(G,1);
+
+    if isnumeric(ord)
+         rX = r.^((ord-2)/2).*X; 
+         G = (rX'*rX)*Rinv;
+    else
+        rX = sign(r).*dnl(r).*X./(abs(r) +eps) ;
+        G = (rX'*X)*Rinv;
+    end
     anew = G'*a;
 %     
      anew = anew./norm(anew);
     d = norm(anew-a);
     
-%     cm(iter+1) = cumulant(X*anew,ord);
+%     cm(iter+1) = cumulant(X*anew,4);
 %     if cm(iter+1)>cm(iter)
         a = anew;
 %     else
@@ -70,3 +85,4 @@ A(:,dim) = a;
  X = X*(eye(size(X,2))-a*a');
 end
 A = pinv(WM)*A;
+A = A.*sign(mean(nl(X0*A)));
