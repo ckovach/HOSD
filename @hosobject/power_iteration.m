157364bd2c819ae290da672581c8bac3d27978a7
ckovach <ckovach@4e33aaf1-3bdf-43e8-baf7-e7e736085cc8>
2022-04-26 19:48:16 +0000

    Added missing method
    git-svn-id: https://saccade.neurosurgery.uiowa.edu/svn/KovachToolbox/trunk/BSTD@1840 4e33aaf1-3bdf-43e8-baf7-e7e736085cc8

---
+++ @hosobject/power_iteration.m
@@ -0,0 +1,21 @@
+function power_iteration(me,X)
+
+% Refine the feature and filter estimates obtained with HOSD through a power iteration method.
+% The ability of HOSD to recover a moment-maximizing filter is only
+% approximate, but it yields a starting estimate which can be quickly
+% refined using a power iteration. This is implemented by updating the detection
+% filter with the partial delay filter computed for the current feature estimate, 
+% then recomputing the feature by aligning on the outputs of the new
+% feature.
+
+
+pdfilt =  me.partial_delay_filt(me.feature);
+
+me.filterfft =pdfilt;
+% delt = me.delay;
+
+[~,FXsh] = me.apply_filter(X,false);
+
+me.wavefft = mean(FXsh,2);
+
+% me.delay = delt;
