621eea3d5f4698e10d4e0703ccd75d9e7e6517d1
ckovach <ckovach@4e33aaf1-3bdf-43e8-baf7-e7e736085cc8>
2022-11-23 13:42:41 +0000

    Fixed typo in variable name
    git-svn-id: https://saccade.neurosurgery.uiowa.edu/svn/KovachToolbox/trunk/BSTD@1909 4e33aaf1-3bdf-43e8-baf7-e7e736085cc8

--- @mvhosd/get_block.m
+++ @mvhosd/get_block.m
@@ -12,7 +12,7 @@
           initialize = true;
       end
       if nargin < 4 || isempty(makeplot)
-          makeplot = ~isa(xin,'gpuArray');   
+          makeplot = ~isa(in,'gpuArray');   
       end
       if nargin < 3 || isempty(maxiter)
           maxiter = 25;
