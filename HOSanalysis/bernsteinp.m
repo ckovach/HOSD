7e127316334cd3682b5c4f9ac605f026aa76cbd8
ckovach <ckovach@4e33aaf1-3bdf-43e8-baf7-e7e736085cc8>
2023-01-10 17:38:35 +0000

    Accept a scalar input for the 1st argument giving the window size.
    git-svn-id: https://saccade.neurosurgery.uiowa.edu/svn/KovachToolbox/trunk/BSTD@1917 4e33aaf1-3bdf-43e8-baf7-e7e736085cc8

--- HOSanalysis/bernsteinp.m
+++ HOSanalysis/bernsteinp.m
@@ -1,7 +1,10 @@
 function p = bernsteinp(n,order)
 
-x = linspace(0,1,n);
-
+if isscalar(n)
+    x = linspace(0,1,n);
+else
+    x = n;
+end
 for k = 0:order
     
     p(:,k+1) = nchoosek(order,k)*x.^k.*(1-x).^(order-k);
--- HOSanalysis/chebyT.m
+++ HOSanalysis/chebyT.m
@@ -3,13 +3,16 @@ function P = chebyT(n,order)
 % P = chebyT(n,order)
 %Return Chebyshev polynomials up to the given order for a window of size n.
 
-
-P = zeros(n,order+1);
-
-P(:,1)=1;
-if order > 0
-    P(:,2) = linspace(-1,1,n);
+if isscalar(n)
+    P = zeros(n,order+1);
+    if order > 0 
+        P(:,2) = linspace(-1,1,n);
+    end
 else
+    P(:,2) = n;
+end
+P(:,1)=1;
+if order == 0 
     return
 end
 
